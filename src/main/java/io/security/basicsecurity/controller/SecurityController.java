0f2c9abc6c2a8175c641bbe541ff7e570db3d8bf
workerKwon <dmdmdm1322@gmail.com>
2022-04-03 15:16:17 +0900

    인증 정보를 SecurityContextHolder를 통해서 전역적으로 참조할 수 있다. 인증된 정보는 HttpSession에도 저장되기 때문에 HttpSession을 통해서도 호출할 수 있다.

--- src/main/java/io/security/basicsecurity/controller/SecurityController.java
+++ src/main/java/io/security/basicsecurity/controller/SecurityController.java
@@ -1,13 +1,25 @@
 package io.security.basicsecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String page() {
+    public String page(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext attribute =(SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = attribute.getAuthentication();
+        System.out.println("session authentication == securityContext authentication : " + (authentication1 == authentication));
+
         return "home";
     }
 
